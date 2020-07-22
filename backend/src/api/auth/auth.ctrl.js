62ab70555bdb6ec2ff5d1085d279faa876ab5797
bongjoi <bong5176@gmail.com>
2020-07-22 17:25:21 +0900

    로그인 api 구현

--- backend/src/api/auth/auth.ctrl.js
+++ backend/src/api/auth/auth.ctrl.js
@@ -36,7 +36,7 @@ exports.register = async (ctx) => {
     // 응답할 데이터에서 hashedPassword 필드 제거
     ctx.body = user.serialize()
   } catch (err) {
-    ctx.throw(500, e)
+    ctx.throw(500, err)
   }
 }
 
@@ -44,7 +44,33 @@ exports.register = async (ctx) => {
  * @desc    로그인
  * @route   POST /api/auth/login
  */
-exports.login = async (ctx) => {}
+exports.login = async (ctx) => {
+  const { username, password } = ctx.request.body
+
+  // username, password가 없으면 에러 처리
+  if (!username || !password) {
+    ctx.status = 401 // Unauthorized
+    return
+  }
+
+  try {
+    const user = await User.findByUsername(username)
+    // 계정이 존재하지 않으면 에러 처리
+    if (!user) {
+      ctx.status = 401
+      return
+    }
+    const valid = await user.checkPassword(password)
+    // 잘못된 비밀번호
+    if (!valid) {
+      ctx.status = 401
+      return
+    }
+    ctx.body = user.serialize()
+  } catch (err) {
+    ctx.throw(500, err)
+  }
+}
 
 /**
  * @desc    로그인 상태 확인
