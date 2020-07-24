c0bf4deb758d30be683fbc5317064a24c9dea3df
bongjoi <bong5176@gmail.com>
2020-07-24 19:13:21 +0900

    로그인 구현 및 회원 인증 에러 처리하기

--- frontend/src/containers/auth/RegisterForm.js
+++ frontend/src/containers/auth/RegisterForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { changeField, initializeForm, register } from '../../modules/auth'
@@ -6,6 +6,7 @@ import AuthForm from '../../components/auth/AuthForm'
 import { check } from '../../modules/user'
 
 const RegisterForm = ({ history }) => {
+  const [error, setError] = useState(null)
   const dispatch = useDispatch()
   const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.register,
@@ -25,8 +26,16 @@ const RegisterForm = ({ history }) => {
     e.preventDefault()
 
     const { username, password, passwordConfirm } = form
+    // 빈 칸이 존재할 경우
+    if ([username, password, passwordConfirm].includes('')) {
+      setError('빈 칸을 모두 입력하세요.')
+      return
+    }
+    // 비밀번호가 불일치 할 경우
     if (password !== passwordConfirm) {
-      // TODO: 오류 처리
+      setError('비밀번호가 일치하지 않습니다.')
+      changeField({ form: 'register', key: 'password', value: '' })
+      changeField({ form: 'register', key: 'passwordConfirm', value: '' })
       return
     }
     dispatch(register({ username, password }))
@@ -40,8 +49,13 @@ const RegisterForm = ({ history }) => {
   // 회원가입 성공/실패 처리
   useEffect(() => {
     if (authError) {
-      console.log('오류 발생')
-      console.log(authError)
+      // 계정명이 이미 존재할 경우
+      if (authError.response.status === 409) {
+        setError('이미 존재하는 계정입니다.')
+        return
+      }
+      // 기타 이유
+      setError('회원가입 실패')
       return
     }
     if (auth) {
@@ -54,6 +68,8 @@ const RegisterForm = ({ history }) => {
   // user 값이 잘 설정되었는지 확인
   useEffect(() => {
     if (user) {
+      console.log('check API 성공')
+      console.log(user)
       history.push('/')
     }
   }, [user, history])
@@ -64,6 +80,7 @@ const RegisterForm = ({ history }) => {
       form={form}
       onChange={onChange}
       onSubmit={onSubmit}
+      error={error}
     />
   )
 }
