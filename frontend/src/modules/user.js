c4269ec06bf8761eba4101f6b579186bace231c1
bongjoi <bong5176@gmail.com>
2020-07-27 14:37:22 +0900

    로그아웃 기능 구현

--- frontend/src/modules/user.js
+++ frontend/src/modules/user.js
@@ -1,5 +1,5 @@
 import { createAction, handleActions } from 'redux-actions'
-import { takeLatest } from 'redux-saga/effects'
+import { takeLatest, call } from 'redux-saga/effects'
 import * as authAPI from '../lib/api/auth'
 import createRequestSaga, {
   createRequestActionTypes
@@ -10,9 +10,11 @@ const TEMP_SET_USER = 'user/TEMP_SET_USER' // 새로고침 이후 임시 로그�
 const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] = createRequestActionTypes(
   'user/CHECK'
 )
+const LOGOUT = 'user/LOGOUT'
 
 export const tempSetUser = createAction(TEMP_SET_USER, (user) => user)
 export const check = createAction(CHECK)
+export const logout = createAction(LOGOUT)
 
 const checkSaga = createRequestSaga(CHECK, authAPI.check)
 function checkFailureSaga() {
@@ -22,10 +24,19 @@ function checkFailureSaga() {
     console.log('localStorage is not working')
   }
 }
+function* logoutSaga() {
+  try {
+    yield call(authAPI.logout)
+    localStorage.removeItem('user')
+  } catch (err) {
+    console.log(err)
+  }
+}
 
 export function* userSaga() {
   yield takeLatest(CHECK, checkSaga)
   yield takeLatest(CHECK_FAILURE, checkFailureSaga)
+  yield takeLatest(LOGOUT, logoutSaga)
 }
 
 const initialState = {
@@ -48,6 +59,10 @@ export default handleActions(
       ...state,
       user: null,
       checkError: error
+    }),
+    [LOGOUT]: (state) => ({
+      ...state,
+      user: null
     })
   },
   initialState
