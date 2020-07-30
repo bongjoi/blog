4507824d50262c33f70e46c1da498d7c0af946ef
bongjoi <bong5176@gmail.com>
2020-07-30 17:21:49 +0900

    write 리덕스 모듈 작성, EditorContainer 만들기

--- frontend/src/modules/index.js
+++ frontend/src/modules/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import { all } from 'redux-saga/effects'
 import auth, { authSaga } from './auth'
 import user, { userSaga } from './user'
+import write from './write'
 import loading from './loading'
 
 const rootReducer = combineReducers({
   auth,
   user,
+  write,
   loading
 })
 
