56747f2c9704f2a6f6fce188378fab2deefdc6e5
bongjoi <bong5176@gmail.com>
2020-07-31 11:05:47 +0900

    PostActionButtons 컴포넌트 만들기

---
+++ frontend/src/components/post/PostActionButtons.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import styled from 'styled-components'
+import palette from '../../lib/styles/palette'
+
+const PostActionButtonsBlock = styled.div`
+  margin-top: -1.5rem;
+  margin-bottom: 2rem;
+  display: flex;
+  justify-content: flex-end;
+`
+
+const ActionButton = styled.button`
+  padding: 0.25rem 0.5rem;
+  font-size: 0.875rem;
+  font-weight: bold;
+  color: ${palette.gray[6]};
+  border: none;
+  border-radius: 4px;
+  outline: none;
+  cursor: pointer;
+  &:hover {
+    background: ${palette.gray[1]};
+    color: ${palette.cyan[7]};
+  }
+  & + & {
+    margin-left: 0.25rem;
+  }
+`
+
+const PostActionButtons = () => {
+  return (
+    <PostActionButtonsBlock>
+      <ActionButton>수정</ActionButton>
+      <ActionButton>삭제</ActionButton>
+    </PostActionButtonsBlock>
+  )
+}
+
+export default PostActionButtons
