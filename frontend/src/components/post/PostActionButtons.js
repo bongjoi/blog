a457afceb995cf38f2cad906f88f9ce558ba621b
bongjoi <bong5176@gmail.com>
2020-07-31 12:32:53 +0900

    포스트 삭제 구현

--- frontend/src/components/post/PostActionButtons.js
+++ frontend/src/components/post/PostActionButtons.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import styled from 'styled-components'
+import AskRemoveModal from './AskRemoveModal'
 import palette from '../../lib/styles/palette'
 
 const PostActionButtonsBlock = styled.div`
@@ -27,12 +28,32 @@ const ActionButton = styled.button`
   }
 `
 
-const PostActionButtons = ({ onEdit }) => {
+const PostActionButtons = ({ onEdit, onRemove }) => {
+  const [modal, setModal] = useState(false)
+
+  const onRemoveClick = () => {
+    setModal(true)
+  }
+  const onCancel = () => {
+    setModal(false)
+  }
+  const onConfirm = () => {
+    setModal(false)
+    onRemove()
+  }
+
   return (
-    <PostActionButtonsBlock>
-      <ActionButton onClick={onEdit}>수정</ActionButton>
-      <ActionButton>삭제</ActionButton>
-    </PostActionButtonsBlock>
+    <>
+      <PostActionButtonsBlock>
+        <ActionButton onClick={onEdit}>수정</ActionButton>
+        <ActionButton onClick={onRemoveClick}>삭제</ActionButton>
+      </PostActionButtonsBlock>
+      <AskRemoveModal
+        visible={modal}
+        onConfirm={onConfirm}
+        onCancel={onCancel}
+      />
+    </>
   )
 }
 
