3a15fd6f4ef3a199efa156274815d3879e388c4a
bongjoi <bong5176@gmail.com>
2020-09-14 20:35:52 +0900

    prepare for deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,9 @@ const Koa = require('koa')
 const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser')
 const mongoose = require('mongoose')
+const serve = require('koa-static')
+const path = require('path')
+const send = require('koa-send')
 
 const api = require('./api')
 const jwtMiddleware = require('./lib/jwtMiddleware')
@@ -13,7 +16,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   })
   .then(() => {
     console.log('Connected to MongoDB')
@@ -34,6 +37,16 @@ app.use(jwtMiddleware)
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods())
+
+const buildDirectory = path.resolve(__dirname, '../../frontend/build')
+app.use(serve(buildDirectory))
+app.use(async (ctx) => {
+  // Not Found이고, 주소가 /api로 시작하지 않는 경우
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    // index.html 내용을 반환
+    await send(ctx, 'index.html', { root: buildDirectory })
+  }
+})
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
